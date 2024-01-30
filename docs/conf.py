4ee02c12838251fceab578e044a98e0ef7523204
arjunsavel <asavel@gmail.com>
2024-01-30 16:12:47 -0500

    now use the same theme that astropy does.

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"
+# html_theme = "sphinx_book_theme"
+html_theme = "pydata-sphinx-theme"
 # html_favicon = "_static/favicon.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
