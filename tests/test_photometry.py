f2568e255b1d8c364e585c7ef61b722756b7c942
kelle <kellecruz@gmail.com>
2024-02-20 17:18:32 -0500

    photometry functions and tests

---
+++ tests/test_photometry.py
@@ -0,0 +1,61 @@
+import pytest
+import logging
+from astrodb_scripts import AstroDBError, load_astrodb
+from astrodb_scripts.photometry import (
+    ingest_photometry,
+    ingest_photometry_filter,
+    fetch_svo,
+    assign_ucd,
+)
+
+# TODO: Write tests for ingest_photometry
+# TODO: Write tests for ingest_photometry_filter
+
+
+logger = logging.getLogger("SIMPLE")
+logger.setLevel(logging.DEBUG)
+
+
+# load the database created by test_1_create_db.py
+DB_NAME = "tests/testdb.sqlite"
+db = load_astrodb(DB_NAME, recreatedb=False)
+
+
+@pytest.mark.parametrize(
+    "telescope, instrument, filter_name, wavelength",
+    [("HST", "WFC3_IR", "F140W", 13734.66)],
+)
+def test_fetch_svo(telescope, instrument, filter_name, wavelength):
+    filter_id, wave, fwhm = fetch_svo(telescope, instrument, filter_name)
+    assert wave.unit == "Angstrom"
+    assert wave.value == pytest.approx(wavelength)
+
+
+def test_fetch_svo_fail():
+    with pytest.raises(AstroDBError) as error_message:
+        fetch_svo("HST", "WFC3", "F140W")
+    assert "not found in SVO" in str(error_message.value)
+
+
+@pytest.mark.parametrize(
+    "wave, ucd",
+    [
+        (100, None),
+        (3001, "em.opt.U"),
+        (4500, "em.opt.B"),
+        (5500, "em.opt.V"),
+        (6500, "em.opt.R"),
+        (8020, "em.opt.I"),
+        (12000, "em.IR.J"),
+        (16000, "em.IR.H"),
+        (22000, "em.IR.K"),
+        (35000, "em.IR.3-4um"),
+        (45000, "em.IR.4-8um"),
+        (85000, "em.IR.8-15um"),
+        (100000, "em.IR.8-15um"),
+        (200000, "em.IR.15-30um"),
+        (500000, None),
+    ],
+)
+def test_assign_ucd(wave, ucd):
+    assert assign_ucd(wave) == ucd
