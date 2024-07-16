d7bf5e32c17e980b4dc85aed03a531557227aa35
kelle <kellecruz@gmail.com>
2024-07-16 17:04:28 -0400

    start of revamp

--- astrodb_utils/fits.py
+++ astrodb_utils/fits.py
@@ -2,9 +2,7 @@ from astropy.io import fits
 
 
 def make_spectrum_header(wavelength_data, **original_header_dict):
-    """Creates a header from a dictionary of values.
-    Based on the IVOA Data Model 1.2. 
-    https://www.ivoa.net/documents/SpectrumDM/20111120/REC-SpectrumDM-1.1-20111120.pdf
+    """Creates a mostly empty header template for a fits file containing a spectrum
 
     TODO: Check that RA and Dec are in degrees
 
@@ -22,37 +20,33 @@ def make_spectrum_header(wavelength_data, **original_header_dict):
 
     """
 
-    mandatory_keywords = [
-        ("VOCLASS", "VO Data Model"),
+    keywords = [
         ("VOPUB", ""),
+        ("VOREF", ""),
         ("TITLE", "Dataset title "),
         ("OBJECT", "Name of observed object"),
         ("RA", "[deg] Pointing position"),
         ("DEC", "[deg] Pointing position"),
+        ("INSTRUME",""),
+        ("TELESCOP",""),
+        ("OBSERVAT",""),
+        ("AUTHOR","")
         ("APERATURE", "[arcsec] Slit width"),
+        ("DATE-OBS", "Date of observation"),
         ("TMID", "[d] MJD of exposure mid-point"),
         ("TELAPSE", "[s] Total elapsed time (s)"),
         ("SPEC_VAL", "[angstrom] Characteristic spectral coordinate"),
         ("SPEC_BW", "[angstrom] width of spectrum"),
-        ("TDMIN1", ""),
-        ("TDMAX1", ""),
-        ("TTYPE1", ""),
-        ("TTYPE2", "")
-    ]
-     
-    recommended_keywords = [
-        ("VOREF",""),
-        ("INSTRUME",""),
-        ("AUTHOR","")
+        ("SPECBAND", "SED.bandpass"),
+        ("APERTURE", "[arcsec] slit width")
+
     ]
 
+    # Make new, blank header
     new_header = fits.Header()
-    # add original_header to new header
 
-    keywords_given = list(original_header_dict.keys())
-
-    # Loop through and assign all mandatory keywords 
-    for key,comment in mandatory_keywords:
+    # Loop through and check if any keywords are in the original header
+    for key,description in keywords:
         value = original_header_dict.get(key)
         if value is not None:
             new_header.set(key, value, description)
@@ -64,49 +58,8 @@ def make_spectrum_header(wavelength_data, **original_header_dict):
 
 
     
-    # try:
-    #    header.set("VOCLASS", original_header_dict["voclass"], "VO Data Model")
-    # except KeyError:
-    #    logging.warning("No VO Data Model")
 
-    # REQUIRED Target Data
-    try:
-        header.set(
-            "OBJECT", original_header_dict["object_name"], "Name of observed object"
-        )
-    except KeyError:
-        object_name = input("REQUIRED: Please input a name for the object: ")
-        header.set("OBJECT", object_name, "Name of observed object")
-
-    try:
-        telescope = original_header_dict["telescope"]
-        header.set("TELESCOP", telescope, "name of telescope")
-    except KeyError:
-        telescope = input("REQURIED: Please input the name of the telescope: ")
-        header.set("TELESCOP", telescope, "name of telescope")
-
-    try:
-        instrument = original_header_dict["instrument"]
-        header.set("INSTRUME", instrument, "name of instrument")
-    except KeyError:
-        instrument = input("REQUIRED: Please input the name of the instrument: ")
-        header.set("INSTRUME", instrument, "name of instrument")
-
-    try:
-        ra = original_header_dict["RA"]
-        header.set("RA_OBJ", ra, "[deg] Right Ascension of object")
-    except KeyError:
-        ra = input(
-            "REQUIRED: Please input the right ascension of the object in degrees: "
-        )
-        header.set("RA_OBJ", ra, "[deg] Right Ascension of object")
-
-    try:
-        dec = original_header_dict["dec"]
-        header.set("DEC_OBJ", dec, "[deg] Declination of object")
-    except KeyError:
-        dec = input("REQUIRED: Please input the declination of the object in degrees: ")
-        header.set("DEC_OBJ", dec, "[deg] Declination of object")
+   
 
     try:
         obs_date = dateparser.parse(original_header_dict["obs_date"]).strftime(
@@ -125,47 +78,13 @@ def make_spectrum_header(wavelength_data, **original_header_dict):
     width = (w_max - w_min).astype(np.single)
     w_mid = ((w_max + w_min) / 2).astype(np.single)
 
-    header.set("SPEC_VAL", w_mid, f"[{w_units}] Characteristic spec coord")
-    header.set("SPEC_BW", width, f"[{w_units}] Width of spectrum")
-    header.set("TDMIN1", w_min, f"[{w_units}] Starting wavelength")
-    header.set("TDMAX1", w_max, f"[{w_units}] Ending wavelength")
+    new_header.set("SPEC_VAL", w_mid, f"[{w_units}] Characteristic spec coord")
+    new_header.set("SPEC_BW", width, f"[{w_units}] Width of spectrum")
+    new_header.set("TDMIN1", w_min, f"[{w_units}] Starting wavelength")
+    new_header.set("TDMAX1", w_max, f"[{w_units}] Ending wavelength")
 
-    try:
-        bandpass = original_header_dict["bandpass"]
-        header.set("SPECBAND", bandpass, "SED.bandpass")
-    except KeyError:
-        bandpass = assign_ucd(w_mid * wavelength_data.unit)
-        header.set("SPECBAND", bandpass, "SED.bandpass")
+   
 
-    # OPTIONAL Header keywords
-    try:
-        exposure_time = original_header_dict["exposure_time"]
-        header.set("TELAPSE", exposure_time, "[s] Total elapsed time")
-    except KeyError:
-        exposure_time = input("Please input the exposure time in seconds: ")
-        if exposure_time != "":
-            header.set("TELAPSE", exposure_time, "[s] Total elapsed time")
-
-    try:
-        time_start = Time(dateparser.parse(original_header_dict["start_time"])).jd
-        header.set("TSTART", time_start, "[d] MJD start time")
-    except KeyError:
-        time_start = None
-
-    try:
-        time_stop = Time(dateparser.parse(original_header_dict["stop_time"])).jd
-        header.set("TSTOP", time_stop, "[d] MJD stop time")
-    except KeyError:
-        time_stop = None
-
-    try:
-        time = (
-            Time(dateparser.parse(original_header_dict["start_time"])).jd
-            + Time(dateparser.parse(original_header_dict["stop_time"])).jd
-        ) / 2
-        header.set("TMID", time, "[d] MJD mid expsoure")
-    except KeyError:
-        time = None
 
     try:
         obs_location = original_header_dict["observatory"]
@@ -173,33 +92,10 @@ def make_spectrum_header(wavelength_data, **original_header_dict):
     except KeyError:
         obs_location = None
 
-    try:
-        aperture = original_header_dict["aperture"]
-        header.set("APERTURE", aperture, "[arcsec] slit width")
-    except KeyError:
-        aperture = input("OPTIONAL: Please input the slitwidth in arcseconds: ")
-        if aperture != "":
-            header.set("APERTURE", aperture, "[arcsec] slit width")
+    
 
-    # Publication Information
-    try:
-        title = original_header_dict["title"]  # trim so header wraps nicely
-        header.set("TITLE", title, "Data set title")
-    except KeyError:
-        title = None
-
-    try:
-        header.set("AUTHOR", original_header_dict["author"], "Authors of the data")
-    except KeyError:
-        author = input("OPTIONAL: Please input the original authors of the data: ")
-        if author != "":
-            header.set("AUTHOR", author, "Authors of the data")
-
-    try:
-        header.set("VOREF", original_header_dict["bibcode"], "Bibcode of dataset")
-    except KeyError:
-        pass
-
+   
+   
     try:
         header.set("REFERENC", original_header_dict["doi"], "DOI of dataset")
     except KeyError:
