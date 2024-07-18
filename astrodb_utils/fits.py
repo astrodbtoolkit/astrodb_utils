406543502a99e81e3a7028b92e211e4ff7d33f8d
kelle <kellecruz@gmail.com>
2024-07-18 14:16:42 -0400

    get_keywords, add_wavengeth functions

--- astrodb_utils/fits.py
+++ astrodb_utils/fits.py
@@ -1,8 +1,9 @@
 import numpy as np
 from astropy.io import fits
+from photometry import assign_ucd
 
 
-def missing_keywords(header=None, *,keywords=None):
+def add_missing_keywords(header=None, *,format='simple-spectrum',keywords=None):
     """Finds the keywords that are missing from a header
 
     Inputs
@@ -10,94 +11,161 @@ def missing_keywords(header=None, *,keywords=None):
     header: fits.Header
         a fits header object or dictionary of header values
 
+    format: string
+        header schemas to enforce. options, 'simple-spectrum'. Eventually, `IVOA-spectrumdm-1.2`
+        if provided, keywords is ignored
+
     keywords: list
-        a list of keywords to check for
+        a list of keywords to check for if format is not specified
 
     Returns
     -------
-    a dictionary of missing keywords and their comments
+    FITS header object
+
+    Examples
+    --------
+    Returns a header with keywords but blank values
+    >>> new_header = add_missing_keywords(format='simple-spectrum')
+
+    Adds missing keywords (with  blank values) to existing header
+    >>> new_header = add_missing_keywords(old_header, format='simple-spectrum')
+
 
     """
+
+    # If no header was provided, start with a blank one
     if header is None:
         header = fits.Header()
 
-    if keywords is None:
-        # keywords to check for by default
-        keywords = [
-            ("VOPUB", ""),
-            ("VOREF", ""),
-            ("TITLE", "Dataset title "),
-            ("OBJECT", "Name of observed object"),
-            ("RA", "[deg] Pointing position"),
-            ("DEC", "[deg] Pointing position"),
-            ("INSTRUME",""),
-            ("TELESCOP",""),
-            ("OBSERVAT",""),
-            ("AUTHOR",""),
-            ("DATE-OBS", "Date of observation"),
-            ("TMID", "[d] MJD of exposure mid-point"),
-            ("TELAPSE", "[s] Total elapsed time (s)"),
-            ("SPEC_VAL", "[angstrom] Characteristic spectral coordinate"),
-            ("SPEC_BW", "[angstrom] width of spectrum"),
-            ("SPECBAND", "SED.bandpass"),
-            ("APERTURE", "[arcsec] slit width")
-    ]
+    if keywords is None and format is None:
+        format = 'simple-spectrum'
 
-    missing_keywords = fits.Header()
+    keywords = get_keywords(format)
 
-    # Loop through original header and add values to new header
+    missing_keywords = []
+    # Loop through original header and add keywords with blank values to new header
     for keyword, comment in keywords:
         value = header.get(keyword)
         if value is None:
-            missing_keywords.set(keyword, None, comment)
+            header.set(keyword, None, comment)
+            missing_keywords.append((keyword,comment))
     
     # Loop over missing keywords and print for copy and paste purposes
     print("COPY AND PASTE THE FOLLOWING COMMAND INTO YOUR SCRIPT")
     print("Fill in the values you have after the colon (:)")
     print("new_header_keywords_dict =")
-    for card in missing_keywords:
-        #print(f" \'{card.keyword}\':             ,  #  {card.comment}")
-        print(card)
-    
-    return missing_keywords
+    for keyword,comment in missing_keywords:
+        print(f"header.set({keyword}, <value>) # {comment}")
 
-def make_spectrum_header(wavelength_data, **original_header_dict):
-    """Creates a mostly empty header template for a fits file containing a spectrum
+    return header
 
-    TODO: if original_header_dict is not provided, what happens?
-    TODO: Check that RA and Dec are in degrees
+
+def add_wavelength_keywords(header=None, wavelength_data = None):
+    """Uses wavelength array to generate header keywords
 
     Inputs
     -------
     wavelength_data: astropy.units.Quantity
         an array of wavelengths. should include units.
 
-    header_dict: dict
-        a dictionary or Header object of values to be included in the header
+    header_dict: header
+        a Header object 
 
     Returns
     -------
-    a fits header dictionary
+    a fits header dictionary with wavelegth keywords added
+
+    Examples
+    --------
+    >>> wavelength = 
+    >>> header = add_wavelength_keywords(wavelength_data = wavelength)
 
     """
 
     # Make new, blank header
-    new_header = fits.Header()
+    if header is None:
+        header = fits.Header()
 
     # Use wavelength data to calculate header values
     w_min = min(wavelength_data).astype(np.single)
     w_max = max(wavelength_data).astype(np.single)
     width = (w_max - w_min).astype(np.single)
     w_mid = ((w_max + w_min) / 2).astype(np.single)
+    bandpass = assign_ucd(w_mid)
 
-    new_header.set("SPEC_VAL", w_mid.value, f"[{w_mid.unit}] Characteristic spec coord")
-    new_header.set("SPEC_BW", width.value, f"[{width.unit}] Width of spectrum")
-    new_header.set("TDMIN1", w_min.value, f"[{w_min.unit}] Starting wavelength")
-    new_header.set("TDMAX1", w_max.value, f"[{w_max.unit}] Ending wavelength")
+    header.set("SPEC_VAL", w_mid.value, f"[{w_mid.unit}] Characteristic spec coord")
+    header.set("SPEC_BW", width.value, f"[{width.unit}] Width of spectrum")
+    header.set("SPECBAND", bandpass )
+    header.set("TDMIN1", w_min.value, f"[{w_min.unit}] Starting wavelength")
+    header.set("TDMAX1", w_max.value, f"[{w_max.unit}] Ending wavelength")
    
-    
+    return header    
 
-    # try:
+   
+
+
+def check_header(header=None, format=None)
+    # check for missing keywords
+    # check RA and Dec are in degrees
+    # check date can be turned to dateTime object
+    # validate the header 
+
+
+def get_keywords(format):
+
+    formats = ['simple-spectrum','ivoa-spectrum-dm-1.2']
+    if format not in formats:
+        msg = f"(Format must be one of these: {formats})"
+        raise ValueError(msg)
+
+    if format == 'simple-spectrum':
+        keywords = [
+            ("TITLE", "Dataset title "),
+            ("OBJECT", "Name of observed object"),
+            ("RA", "[deg] Pointing position"),
+            ("DEC", "[deg] Pointing position"),
+            ("INSTRUME", "Instrument name"),
+            ("TELESCOP", "Telescope name"),
+            ("AUTHOR", ""),
+            ("CONTRIB1","Contributor who generated this file"),
+            ("REFERENC","DOI of original publication"),
+            ("VOPUB", "Publisher") # TODO: Set to SIMPLE
+            ("DATE-OBS", "Date of observation"),
+            ("TELAPSE", "[s] Total elapsed time (s)"),
+            ("SPEC_VAL", "[angstrom] Characteristic spectral coordinate"),
+            ("SPEC_BW", "[angstrom] width of spectrum"),
+            ("SPECBAND", "SED.bandpass"),
+            ("APERTURE", "[arcsec] slit width")
+        ]
+    elif format == 'ivoa-spectrum-dm-1.2':    
+        keywords = [
+            ("VOCLASS","Data model name and version") # TODO:  'Spectrum-1.2', 
+            ("VOPUB", ""),
+            ("VOREF", ""),
+            ("TITLE", "Dataset title "),
+            ("OBJECT", "Name of observed object"),
+            ("RA_TARG", "[deg] Pointing position"),
+            ("DEC_TARG", "[deg] Pointing position"),
+            ("INSTRUME", ""),
+            ("TELESCOP", ""),
+            ("OBSERVAT", ""),
+            ("AUTHOR", ""),
+            ("CONTRIB1","Contributor who generated this file")
+            ("DATE-OBS", "Date of observation"),
+            ("TMID", "[d] MJD of exposure mid-point"),
+            ("TELAPSE", "[s] Total elapsed time (s)"),
+            ("SPEC_VAL", "[angstrom] Characteristic spectral coordinate"),
+            ("SPEC_BW", "[angstrom] width of spectrum"),
+            ("TDMIN1","Start in spectral coordinate"),
+            ("TDMAX1","Stop in spectral coordinate")
+            ("SPECBAND", "SED.bandpass"),
+            ("APERTURE", "[arcsec] slit width"),
+        ]
+
+    return keywords
+
+
+     # try:
     #     obs_date = dateparser.parse(original_header_dict["obs_date"]).strftime(
     #         "%Y-%m-%d"
     #     )
@@ -112,15 +180,6 @@ def make_spectrum_header(wavelength_data, **original_header_dict):
     # except KeyError:
     #     pass
 
-    # try:
-    #     header.set("COMMENT", comment)
-    # except KeyError:
-    #     pass
-
-    # try:
-    #     header.set("HISTORY", history)
-    # except KeyError:
-    #     pass
-
+    # Should be done by specutils
     # header.set("DATE", date.today().strftime("%Y-%m-%d"), "Date of file creation")
     # header.set("CREATOR", "simple.spectra.convert_to_fits.py", "FITS file creator")
