8d45e185599e52b23e9fc614d97dc18887c07f59
kelle <kellecruz@gmail.com>
2024-07-29 14:01:30 -0500

    fixed tests

--- astrodb_utils/tests/test_spectra.py
+++ astrodb_utils/tests/test_spectra.py
@@ -16,10 +16,9 @@ from astrodb_utils.spectra import (
     [
         ("./astrodb_utils/tests/data/2MASS+J21442847+1446077.fits", True),
         ("./astrodb_utils/tests/data/U50184_1022+4114_HD89744B_BUR08B.fits", False),
-        ("./astrodb_utils/tests/data/U50185_GJ1048B_0235-2331_BUR08B.fits", False),
     ],
 )
 def test_check_spectrum_class(spectrum_path, result):
-    assert os.path.exists(spectrum_path) == True
+    assert os.path.exists(spectrum_path) is True
     check = check_spectrum_class(spectrum_path, raise_error=False)
     assert check == result
