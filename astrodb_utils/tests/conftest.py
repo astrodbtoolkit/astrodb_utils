ebc68726167795c1301f70c5c146b357e473274b
kelle <kellecruz@gmail.com>
2025-02-11 10:20:06 -0500

    fix for switch to felis schema definition

--- astrodb_utils/tests/conftest.py
+++ astrodb_utils/tests/conftest.py
@@ -1,15 +1,21 @@
-import logging
 import os
 import sys
 
 import pytest
 
+from astrodb_utils import load_astrodb, logger
+
+REFERENCE_TABLES = [
+    "Publications",
+    "Telescopes",
+    "Instruments",
+    "PhotometryFilters",
+    "Versions",
+    "Regimes"
+]
+
+
 sys.path.append("./tests/astrodb-template-db/")
-from schema.schema_template import REFERENCE_TABLES
-
-from astrodb_utils import load_astrodb
-
-logger = logging.getLogger("AstroDB")
 
 DB_NAME = "tests/test-template-db.sqlite"
 DB_PATH = "tests/astrodb-template-db/data"
@@ -26,6 +32,6 @@ def db():
     # Confirm file was created
     assert os.path.exists(DB_NAME)
 
-    logger.info("Loaded SIMPLE database using db function in conftest")
+    logger.info("Loaded SIMPLE database using load_astrodb function in conftest.py")
 
     return db
