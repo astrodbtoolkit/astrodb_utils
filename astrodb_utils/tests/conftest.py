e073fd72156b5b9d04d410b4e8cf52da333aeaee
kelle <kellecruz@gmail.com>
2025-03-10 11:09:18 -0400

    add some data to be used in all tests

--- astrodb_utils/tests/conftest.py
+++ astrodb_utils/tests/conftest.py
@@ -4,6 +4,8 @@ import sys
 import pytest
 
 from astrodb_utils import load_astrodb, logger
+from astrodb_utils.publications import ingest_publication
+from astrodb_utils.sources import ingest_source
 
 logger.setLevel("DEBUG")
 
@@ -24,6 +26,20 @@ def db():
     # Confirm file was created
     assert os.path.exists(DB_NAME)
 
-    logger.info("Loaded SIMPLE database using load_astrodb function in conftest.py")
+    logger.info("Loaded AstroDB Template database using load_astrodb function in conftest.py")
+
+    ingest_publication(db, doi="10.1086/161442") # Prob83
+
+    ingest_publication(
+        db,
+        reference="Refr20",
+        bibcode="2020MNRAS.496.1922B",
+        doi="10.1093/mnras/staa1522",
+        ignore_ads=True,
+    )
+
+    ingest_source(db, "LHS 2924", reference="Prob83")
 
     return db
+
+
