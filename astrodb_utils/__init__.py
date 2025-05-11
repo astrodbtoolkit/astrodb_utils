3237a6856c2d140b90972eb1395fbbad07495146
Kelle Cruz <kellecruz@gmail.com>
2025-05-11 18:08:01 -0400

    Check_spectrum_plottable fix for JWST data in Jy (#151)
    * add optional format
    
    * better test for flux units
    
    * improvements to ingest_instrument
    
    * check_spectrum_plottable test
    
    * log message tweak
    
    * name the main logger
    
    * Spectrum1D -> Spectrum
    
    * delete dead code
    
    * require specutils>1.20.1
    
    * remove check_spectrum_class helper

--- astrodb_utils/__init__.py
+++ astrodb_utils/__init__.py
@@ -15,7 +15,7 @@ __all__ = ["__version__"]
 
 
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("astrodb_utils")
 
 LOGFORMAT = logging.Formatter(
     "%(levelname)-8s - %(name)-15s - %(message)s")
@@ -30,7 +30,7 @@ handler = logging.StreamHandler(stream=sys.stdout)
 handler.setFormatter(LOGFORMAT)
 logger.addHandler(handler)
 
-logger.info("astrodb_utils logger initialized")
+logger.info(f"Logger initialized: {logger.name}")
 logger.info(f"Logger level: {logging.getLevelName(logger.getEffectiveLevel()) }")
 
 warnings.filterwarnings("ignore", module="astroquery.simbad")
